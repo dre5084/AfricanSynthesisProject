91bc3fef1316007e9a8273835d981a04948df28d
Ondřej Mottl <ondrej.mottl@gmail.com>
2025-05-17 20:26:55 +0200

    East afrika mvgam: add model to fit using mvgam paackage (does not work)

--- R/03_Model_temporal_trends/East_afrika/05_evaluate_mvgam_mod_east_afrika.R
+++ R/03_Model_temporal_trends/East_afrika/05_evaluate_mvgam_mod_east_afrika.R
@@ -3,60 +3,58 @@ library(RUtilpol)
 library(mvgam)
 
 
-hg_model <-
+mod_mvgam_gp <-
   RUtilpol::get_latest_file(
-    file_name = "hg_model",
-    dir = here::here("Data/Processed/")
+    file_name = "mod_mvgam_gp",
+    dir = here::here("Data/Processed/Models/East_afrika/")
   )
 
 # Diagnostics
 summary(
-  hg_model,
+  mod_mvgam_gp,
   include_betas = FALSE,
   smooth_test = FALSE
 )
 
-plot(hg_model)
+plot(mod_mvgam_gp, series = 1)
 
-
-mvgam:::plot.mvgam(hg_model, type = "series", series = 1)
-mvgam:::plot.mvgam(hg_model, type = "pterms")
-mvgam:::plot.mvgam(hg_model, type = "trend", series = 1)
+mvgam:::plot.mvgam(mod_mvgam_gp, type = "series", series = 1)
+mvgam:::plot.mvgam(mod_mvgam_gp, type = "pterms")
+mvgam:::plot.mvgam(mod_mvgam_gp, type = "trend", series = 1)
 
 
 mvgam::mcmc_plot(
-  hg_model,
+  mod_mvgam_gp,
   type = "rhat_hist"
 )
 
 mvgam::mcmc_plot(
-  hg_model,
+  mod_mvgam_gp,
   variable = "sigma",
   regex = TRUE,
-  type = "trace"
+  type = "hist"
 )
 
 # Unconditional posterior check
-mvgam::pp_check(hg_model,
-  type = "dens_overlay_grouped",
+mvgam::pp_check(mod_mvgam_gp,
+  type = "resid_ribbon_grouped",
   group = "series",
   ndraws = 50
 )
 
-
 data_pred <-
   marginaleffects::avg_predictions(
-    hg_model,
+    mod_mvgam_gp,
     by = "time"
   ) %>%
   as.data.frame()
 
 data_pred_indiv <-
   marginaleffects::predictions(
-    hg_model,
+    mod_mvgam_gp,
     newdata = datagrid(
       time = 1:45,
-      series = insight::get_data(hg_model) %>%
+      series = insight::get_data(mod_mvgam_gp) %>%
         dplyr::distinct(series) %>%
         dplyr::pull(series)
     )
@@ -93,13 +91,13 @@ ggplot2::ggplot(
     linewidth = 1
   ) +
   ggplot2::geom_point(
-    data = insight::get_data(hg_model),
+    data = insight::get_data(mod_mvgam_gp),
     ggplot2::aes(
       y = ROC_mean,
       col = series
     )
   ) +
   ggplot2::coord_cartesian(
-    ylim = c(0, 1.25) 
+    ylim = c(0, 1.25)
   ) +
   ggplot2::facet_wrap(~series)
